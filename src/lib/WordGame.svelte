7addc9571041dc4be6ebc4db0778da05224072d3
Charles Spencer Blancas <86479293+Charles-Spencer-Blancas@users.noreply.github.com>
2024-01-27 14:16:40 -0500

    init

---
+++ src/lib/WordGame.svelte
@@ -0,0 +1,27 @@
+<script>
+    import Guess from "./Guess.svelte";
+    let currentGuess = "";
+
+    let onKeyDown = (e) => {
+        let key = e.key;
+        let regex = new RegExp("^[A-Za-z]$");
+
+        if (regex.test(key)) {
+            console.log(key);
+            return;
+        }
+
+        if (key == "Backspace") {
+            console.log("backspace");
+            return;
+        }
+    };
+</script>
+
+<Guess />
+<Guess />
+<Guess />
+<Guess />
+<Guess />
+
+<svelte:window on:keydown|preventDefault={onKeyDown} />
