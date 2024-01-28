4d455a472bff58c93be8e21ec6601c41a295800c
Charles Spencer Blancas <86479293+Charles-Spencer-Blancas@users.noreply.github.com>
2024-01-28 04:32:34 -0500

    make title bigge

--- src/App.svelte
+++ src/App.svelte
@@ -5,24 +5,9 @@
 </script>
 
 <main>
-    <div>Chortle</div>
+    <h1 class="title">Chortle</h1>
     <WordGame />
 </main>
 
 <style>
-    .logo {
-        height: 6em;
-        padding: 1.5em;
-        will-change: filter;
-        transition: filter 300ms;
-    }
-    .logo:hover {
-        filter: drop-shadow(0 0 2em #646cffaa);
-    }
-    .logo.svelte:hover {
-        filter: drop-shadow(0 0 2em #ff3e00aa);
-    }
-    .read-the-docs {
-        color: #888;
-    }
 </style>
